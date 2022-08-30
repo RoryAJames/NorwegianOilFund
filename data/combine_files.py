317fa22b8014d2a814d666c52cbea72ff9dcc622
Rory James <rory.allen.james@gmail.com>
2022-08-30 10:03:40 -0400

    Added category column

--- data/combine_files.py
+++ data/combine_files.py
@@ -9,16 +9,20 @@ extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
 dfs_to_concat = []
+
 for f in all_filenames:
     df = pd.read_csv(f)
     df['file_identifier'] = [f] * len(df.index)
     
     #Create investment category column
+    df['Category'] = df['file_identifier'].apply(lambda x: 'Equity' if x[:2] == 'EQ' else 'Fixed Income')
     
     #Create year column
     
     dfs_to_concat.append(df)
     
 combined_csv = pd.concat(dfs_to_concat)
+
+print(combined_csv.head())
 
 #combined_csv.to_csv("combined.csv", index=False)
