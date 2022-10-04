c2dbaa49b4dab49a6c93007d8b75020ed35b7f0f
Rory James <rory.allen.james@gmail.com>
2022-10-04 12:47:08 -0400

    Combined CSV files to master

--- data/combine_files.py
+++ data/combine_files.py
@@ -14,18 +14,30 @@ for f in all_filenames:
     df = pd.read_csv(f)
     df['file_identifier'] = [f] * len(df.index)
     
-    #Create investment category column by extracting first two characters from file identifier
+    #Create investment category column by extracting the first two characters from file identifier
     df['Category'] = df['file_identifier'].apply(lambda x: 'Equity' if x[:2] == 'EQ' else 'Fixed Income')
     
-    #Create year column
+    #Create year column from file identifier
     df['Year'] = df['file_identifier'].str[3:7].astype(int)
     
     dfs_to_concat.append(df)
-    
+ 
+# Concat dataframes together to create the master
 combined_csv = pd.concat(dfs_to_concat)
-
-print(combined_csv.head())
 
 #Remove unwanted columns
 
-#combined_csv.to_csv("combined.csv", index=False)
+cols_to_drop = ['Market Value(NOK)','Voting','Ownership','Incorporation Country','file_identifier']
+
+combined_csv.drop(columns=cols_to_drop, inplace=True)
+
+#Rename market value column to 'Value'
+
+new_col = {'Market Value(USD)':'Value'}
+combined_csv.rename(columns= new_col, inplace = True)
+
+#Create a CSV output of the master 
+
+combined_csv.to_csv("combined.csv", index=False)
+
+#print(combined_csv.head())
