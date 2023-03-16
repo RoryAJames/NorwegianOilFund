72f715d7d412d3a2ed359ec6badc0a124ca1343b
Rory James <rory.allen.james@gmail.com>
2023-03-16 16:48:05 -0400

    Testing plotly graphs in app

--- apps/historical.py
+++ apps/historical.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import psycopg2
 import pandas as pd
+import plotly.express as px
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
@@ -25,11 +26,21 @@ def show_historical_page():
     st.title('Historical Trends of The Norwegian Oil Fund')
     
     query = """SELECT year as Year,
-ROUND((SUM(CASE WHEN category = 'Equity' THEN market_value ELSE 0 END) / NULLIF(SUM(market_value),0)) * 100, 2) AS "Equity Percentage",
-ROUND((SUM(CASE WHEN category = 'Fixed Income' THEN market_value ELSE 0 END) / NULLIF(SUM(market_value),0)) * 100, 2) AS "Fixed Income Percentage"
-FROM oil_fund
-GROUP BY year;"""
+    ROUND((SUM(CASE WHEN category = 'Equity' THEN market_value ELSE 0 END) / NULLIF(SUM(market_value),0)) * 100, 2) AS "Equity Percentage",
+    ROUND((SUM(CASE WHEN category = 'Fixed Income' THEN market_value ELSE 0 END) / NULLIF(SUM(market_value),0)) * 100, 2) AS "Fixed Income Percentage"
+    FROM oil_fund
+    GROUP BY year;"""
     
     df = run_query(query)
     
-    st.write(df)
+    # create a plotly figure object
+    fig = px.line(df, x='year', y=['Equity Percentage', 'Fixed Income Percentage'], markers=True)
+
+    # update the plotly figure object layout
+    fig.update_layout(title='Equity and Fixed Income Proportions Over Time',
+                      title_x = 0.5,
+                      xaxis_title='Year',
+                      yaxis_title='Percentage Of Fund')
+    
+    # Plot
+    st.plotly_chart(fig, use_container_width=True)
