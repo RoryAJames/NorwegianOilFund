c9b8463e8065873e933be4adffaca167884c3d79
Rory James <rory.allen.james@gmail.com>
2023-03-17 10:31:56 -0400

    Upgraded streamlit to cache database connection

--- apps/historical.py
+++ apps/historical.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-#@st.cache_resource
+@st.cache_resource
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
@@ -13,7 +13,7 @@ conn = init_connection()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-#@st.cache_data(ttl=600)
+@st.cache_data(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
