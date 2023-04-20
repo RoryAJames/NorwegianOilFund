3e3614f38aa770c99e621e7cd47538d59ada4dfb
Rory James <rory.allen.james@gmail.com>
2023-04-20 11:39:59 -0400

    updated eq_fi_proportions SQL file

--- SQL/eq_fi_proportions.sql
+++ SQL/eq_fi_proportions.sql
@@ -1,5 +1,5 @@
-SELECT year,
-ROUND((SUM(CASE WHEN category = 'Equity' THEN market_value ELSE 0 END) / NULLIF(SUM(market_value),0)) * 100, 2) AS "Equity Percentage",
-ROUND((SUM(CASE WHEN category = 'Fixed Income' THEN market_value ELSE 0 END) / NULLIF(SUM(market_value),0)) * 100, 2) AS "Fixed Income Percentage"
+SELECT year as Year,
+ROUND(SUM(market_value) FILTER (WHERE category = 'Equity')/SUM(market_value) * 100.00,2) AS "Equity Proportion",
+ROUND(SUM(market_value) FILTER (WHERE category = 'Fixed Income')/SUM(market_value) *100.00,2) AS "Fixed Income Proportion"
 FROM oil_fund
 GROUP BY year;
