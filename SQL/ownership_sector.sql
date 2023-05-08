ccc80298a8d5f35bb49c173c54e7626a6082e5f3
Rory James <rory.allen.james@gmail.com>
2023-05-08 14:22:04 -0400

    Updated ownership by sector query

--- SQL/ownership_sector.sql
+++ SQL/ownership_sector.sql
@@ -1,5 +1,8 @@
-SELECT year, sector AS "Sector", ROUND(AVG(percent_ownership), 2) AS avg_percent_ownership
+SELECT * 
+FROM 
+(SELECT year, sector AS "Sector", ROUND(AVG(percent_ownership), 2) AS avg_percent_ownership
 FROM oil_fund
-WHERE category = 'Equity'
+WHERE category = 'Equity' 
 GROUP BY year, sector
-ORDER BY year;
+ORDER BY year) as subq
+WHERE avg_percent_ownership >= 0;
